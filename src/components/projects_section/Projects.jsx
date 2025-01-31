0c593da25accf2148a7bb6c74ae90f6cf4144931
EucTech <euccreative@gmail.com>
2025-01-31 10:03:52 -0500

    updated

--- src/components/projects_section/Projects.jsx
+++ src/components/projects_section/Projects.jsx
@@ -1,81 +1,54 @@
 import Project_card from "../project_card/Project_card";
-import first from "./imgs/project1.jpg";
-import second from "./imgs/project2.jpg";
-import third from "./imgs/project3.jpg";
-import forth from "./imgs/project4.jpg";
-import fifth from "./imgs/project5.jpg";
-import sixth from "./imgs/project6.jpg";
-import seventh from "./imgs/project7.jpg";
-import eight from "./imgs/project8.jpg";
-import ninth from "./imgs/project9.jpg";
+
+import shan from "../projects_section/imgs/shan.png";
+import bazzar from "../projects_section/imgs/bazzar.png";
+import creator from "../projects_section/imgs/creator.png";
+import aq from "../projects_section/imgs/aq.png";
+import idea from "../projects_section/imgs/idea.png";
+
 import {useNavigate} from 'react-router-dom'
 
 const Projects = () => {
   // date
   const projects = [
     {
-      img: seventh,
+      img: bazzar,
+      langs: ["TSX", "NEXT JS", "CSS", "TypeScript"],
+      title: "Use Bazzar",
+      disc: "A company landing page buying of food stuffs",
+      link: "https://bazaar-red.vercel.app/"
+    },
+    {
+      img: shan,
+      langs: ["HTML", "CSS", "AJAX", "Django", "Python"],
+      title: "Shan Feng Tire Company",
+      disc: "A full stack web site for a tire company that repaires and sells tires",
+      link: "https://www.shanfengcoltd.com/"
+    },
+    {
+      img: creator,
       langs: ["jsx", "css", "javascript", "react", "tailwind css", "node js"],
-      title: "The farm house club",
-      disc: "A full stack web application for investors in the agricultural industry",
-      link: "https://thefarmhouseclub.io/"
+      title: "Withaudacious",
+      disc: "A website for content creators community",
+      link: "https://withaudacious.vercel.app/"
     },
+
     {
-      img: eight,
-      langs: ["jsx", "react", "css", "javascript"],
-      title: "Heart Felt",
-      disc: "A full stack web appllication for customization and distribution of cards to loved ones",
-      link: "https://new-heartfelt.vercel.app/"
+      img: aq,
+      langs: ["HTML", "CSS", "Javascript", "Next js", "tailwind css"],
+      title: "Aquavital Relief ",
+      disc: "Designed and developed a clean, modern landing page for healthcare.",
+      link: "https://aquavital-relief.vercel.app/"
     },
+
     {
-      img: first,
-      langs: ["jsx", "css", "javascript", "react", "tailwind css", "node js", "express js", "mongo db"],
-      title: "E-Invoice",
-      disc: "A Sass for recording sales and generating of receipts both in pdf format and image",
-      link: "https://invoice-application-three.vercel.app/"
+      img: idea,
+      langs: ["HTML", "CSS", "Javascript", "React", "tailwind css"],
+      title: "Idea Nexus",
+      disc: "Website to pitch an agricultural innovation project.",
+      link: "https://idea-nexus-nine.vercel.app/"
     },
-    {
-      img: ninth,
-      langs: ["html", "css", "javascript", "bootstrap"],
-      title: "The Go travels",
-      disc: "Landing page for an applcation for booking travels and tours",
-      link: "https://thegotravels.com/"
-    },
-    {
-      img: second,
-      langs: ["html", "css", "javascript", "owl carousel"],
-      title: "Homyz",
-      disc: "A landing page for a real estate website",
-      link: "https://frank-real-estate.netlify.app"
-    },
-    {
-      img: third,
-      langs: ["jsx", "css", "javascript", "material UI", "node.js", "express js", "mongo db"],
-      title: "Class Monitor",
-      disc: "A full stack application for taking down my students records, assignments and projects",
-      link: "https://classmonitor.netlify.app/"
-    },
-    {
-      img: forth,
-      langs: ["jsx", "css", "javascript", "node.js", "express js", "mongo db"],
-      title: "Elx Academy",
-      disc: "A full stack application for registering students who wants to learn tech skills",
-      link: "https://elx-academy.netlify.app/"
-    },
-    {
-      img: fifth,
-      langs: ["jsx", "css", "javascript", "node.js", "express js", "mongo db"],
-      title: "My Students Records",
-      disc: "A full stack application for taking down my students records to keep track of their training period",
-      link: "https://mystudentsrecords.netlify.app/"
-    },
-    {
-      img: sixth,
-      langs: ["jsx", "css", "javascript", "node.js", "express js"],
-      title: "Frank AI",
-      disc: "A clone of ChatGpt making use of Open AI's API for the development.",
-      link: "https://frankai.netlify.app"
-    },
+
   ];
 
   const navigate = useNavigate()
